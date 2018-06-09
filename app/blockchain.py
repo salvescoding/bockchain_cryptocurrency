88b8692184f8a36bd5fae623e0f0c63a79c68853
Sergio Alves <salves2383@gmail.com>
2018-06-09 20:28:19 +0200

    Implement method to check balance of coins

--- app/blockchain.py
+++ app/blockchain.py
@@ -13,6 +13,32 @@ def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
 
 
+def coins_received(participant):
+    transaction_sender = [[tx['amount'] for tx in block['transactions']
+                           if tx['receiver'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in transaction_sender:
+        if len(tx) > 0:
+            amount_received += tx[0]
+
+    return amount_received
+
+
+def coins_sent(participant):
+    transaction_sender = [[tx['amount'] for tx in block['transactions']
+                           if tx['sender'] == participant] for block in blockchain]
+    amount_sent = 0
+    for tx in transaction_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+
+    return amount_sent
+
+
+def get_balance(participant):
+    return coins_received(participant) - coins_sent(participant)
+
+
 def last_blockchain_value():
     if len(blockchain) < 1:
         return None
@@ -40,6 +66,7 @@ def mine_block():
              'transactions': open_transactions
              }
     blockchain.append(block)
+    return True
 
 
 def get_transaction_value():
@@ -87,15 +114,16 @@ while waiting_for_input:
         add_transaction(receiver, amount=amount)
         print(open_transactions)
     elif choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif choice == '3':
         print_blockchain_elements()
     elif choice == '4':
         if len(blockchain) >= 1:
             blockchain[0] = {
-              'prv_block_hash': '',
-              'index': 0,
-              'transactions': {'sender': 'Marise', 'receiver': 'Sergio', 'amount': 100.0}
+                'prv_block_hash': '',
+                'index': 0,
+                'transactions': {'sender': 'Marise', 'receiver': 'Sergio', 'amount': 100.0}
             }
     elif choice == '5':
         print(participants)
@@ -107,6 +135,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print("Invalid Blockchain")
         break
+    print(f" Your balance is: {get_balance(owner)} coins")
 else:
     print('Thank you for using our service')
 
