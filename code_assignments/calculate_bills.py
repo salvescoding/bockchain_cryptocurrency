983d2741841a27c6309c9940c5b12d75f00d24ea
Sergio Alves <salves2383@gmail.com>
2018-06-15 19:09:57 +0200

    Code assignment using the python standard library

--- code_assignments/calculate_bills.py
+++ code_assignments/calculate_bills.py
@@ -16,21 +16,12 @@ def calculate_amount_per_day(amount):
     return amount / 60
 
 
-def calculate_individual_bill(total):
-    bill = total / len(flatmates_days)
-    for key in flatmates_total:
-        if key in flatmates_days:
-            flatmates_total[key] += bill
+def calculate_individual_bill(amount, amount_daily):
+    for key in flatmates_days:
+        print(key)
 
 
-def calculate_bill_per_days(amount_of_days, amount_per_day):
-    total = float(amount_of_days) * amount_per_day
-    total = total / float(len(flatmates_days))
-    calculate_individual_bill(total)
 
-
-def flatmate_with_less_days():
-    return min(flatmates_days, key=flatmates_days.get)
 
 
 amount = float(input('Introduce your amount of the bill: '))
@@ -39,13 +30,25 @@ for key, value in flatmates_days.items():
     flatmates_days[key] = days
 
 amount_daily = (calculate_amount_per_day(amount))
-
-while len(flatmates_days) > 0:
-    less_days = flatmate_with_less_days()
-    amount_of_days = flatmates_days[less_days]
-    calculate_bill_per_days(amount_of_days, amount_daily)
-    del flatmates_days[less_days]
-
+calculate_individual_bill(amount, amount_daily)
 
 for key, value in flatmates_total.items():
     print(f'{key}, have to pay: {value}')
+
+
+
+# while len(flatmates_days) > 0:
+#     less_days = flatmate_with_less_days()
+#     amount_of_days = flatmates_days[less_days]
+#     calculate_bill_per_days(amount_of_days, amount_daily)
+#     del flatmates_days[less_days]
+
+# def calculate_bill_per_days(amount_of_days, amount_per_day):
+#     total = float(amount_of_days) * amount_per_day
+#     total = total / float(len(flatmates_days))
+#     calculate_individual_bill(total)
+
+
+# def flatmate_with_less_days():
+#     return min(flatmates_days, key=flatmates_days.get)
+
