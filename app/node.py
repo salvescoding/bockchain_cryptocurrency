4b5137548ca3212e756250d65224d2d5d38680c9
Sergio Alves <salves2383@gmail.com>
2018-06-29 17:56:46 +0200

    Created first route for chain, to display our blockchain as JSON

--- app/node.py
+++ app/node.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from wallet import Wallet
+from blockchain import Blockchain
 
 app = Flask(__name__)
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return 'This worked!'
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    chain_dict = [block.__dict__.copy() for block in chain_snapshot]
+    for block_dict in chain_dict:
+        block_dict['transactions'] = [tx.__dict__ for tx in block_dict['transactions']]
+    return jsonify(chain_dict), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
