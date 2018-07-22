2d6b9416cc31e2d5081fdd4e2cd2a3c0a9b38e92
Sergio Alves <salves2383@gmail.com>
2018-07-22 13:19:23 +0200

    Removed code assignments from project

--- code_assignments/code_assignment_2.py
+++
@@ -1,9 +0,0 @@
-names = ["Sergio", "Alves", "Nuno", "Nambia", "Marise", "Van", "Namagesco"]
-
-for name in names:
-    if len(name) >= 5 and "N" in name:
-        print(name)
-
-while len(names) > 0:
-    names.pop()
-    print(names)
